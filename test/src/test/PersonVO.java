fcaeacb9ec91fbcab793eb783fdbd67dd715b0c4
권민귀 <kmg1249@gmail.com>
2017-12-28 10:42:03 +0900

    hotfix age getter setter

--- test/src/test/PersonVO.java
+++ test/src/test/PersonVO.java
@@ -6,6 +6,12 @@ public class PersonVO {
 	private String tel;
 	private int no;
 	private int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public String getName() {
 		return name;
 	}
